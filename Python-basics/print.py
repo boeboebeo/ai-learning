9474e44f04e095403a9f88915b381f7e8139fc33
boe <boeboebeo@gmail.com>
2026-03-08 00:48:04 +0900

    Day 01 - First Python file

---
+++ Python-basics/print.py
@@ -0,0 +1,13 @@
+#My journey to becoming an Audio ML Engineer starts here!
+
+title = "Blinding Lights"
+artist = "The Weekend"
+bpm = 171 
+    #"171" - prints without str(), but can't calculate
+
+#just print
+print("Title :" + title + " / Artist :" + artist + " / BPM :" + str(bpm))
+print("Title :", title, "/", "artist :", artist, "/", "BPM : ", bpm)
+
+#f string (no str() needed)
+print(f"Title : {title} / artist : {artist} / bpm : {bpm}")
